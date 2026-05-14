32be1c4a13cc18c672cbe057d615cdb082a915e8
fmarti90 <fmarti90@tragsa.es>
2026-05-14 08:20:22 +0200

    Corregimos CursoModuloRepository.
    Se añade el hidden "uriImagen" al formulario para no perder la imagen
    del curso actual en modificaciones.

--- src/main/java/es/alumno/uned/model/repository/CursoModuloRepository.java
+++ src/main/java/es/alumno/uned/model/repository/CursoModuloRepository.java
@@ -3,7 +3,8 @@ package es.alumno.uned.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.alumno.uned.model.entities.CursoModulo;
+import es.alumno.uned.model.entities.CursoModuloId;
 
-public interface CursoModuloRepository extends JpaRepository<CursoModulo, Long> {
+public interface CursoModuloRepository extends JpaRepository<CursoModulo, CursoModuloId> {
 
 }
